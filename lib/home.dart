71718efae38cdb6da3d795a78b362cf8febedac3
zyn10 <zainandcs@gmail.com>
2023-06-25 19:28:05 +0500

    Splash Done

---
+++ lib/home.dart
@@ -0,0 +1,357 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text("Admin Panel"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  backgroundColor: const Color.fromARGB(255, 225, 225, 225),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacement(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //       builder: (context) => const ManagerUser()),
+                  // );
+                },
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.people,
+                      color: Colors.blueGrey,
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          "CP METER LOAD",
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  backgroundColor: const Color.fromARGB(255, 225, 225, 225),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacement(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //       builder: (context) => const PetShowInfo()),
+                  // );
+                },
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.event,
+                      color: Colors.blueGrey,
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          "CP READINGS ",
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  backgroundColor: const Color.fromARGB(255, 225, 225, 225),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacement(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //       builder: (context) => const CreateAlert()),
+                  // );
+                },
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.crisis_alert_sharp,
+                      color: Colors.blueGrey,
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          "MCCB LOAD ",
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  backgroundColor: const Color.fromARGB(255, 225, 225, 225),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacement(
+                  //   context,
+                  //   MaterialPageRoute(builder: (context) => const NGOInfo()),
+                  // );
+                },
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.local_hospital,
+                      color: Colors.blueGrey,
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          "ACDB LOAD ",
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  backgroundColor: const Color.fromARGB(255, 225, 225, 225),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacement(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //       builder: (context) => const PetShowInfo()),
+                  // );
+                },
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.event,
+                      color: Colors.blueGrey,
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          "Rectifier one load ",
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  backgroundColor: const Color.fromARGB(255, 225, 225, 225),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacement(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //       builder: (context) => const PetShowInfo()),
+                  // );
+                },
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.event,
+                      color: Colors.blueGrey,
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          "Rectifier Two load ",
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  backgroundColor: const Color.fromARGB(255, 225, 225, 225),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacement(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //       builder: (context) => const PetShowInfo()),
+                  // );
+                },
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.event,
+                      color: Colors.blueGrey,
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          "OMO 1 Load ",
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  backgroundColor: const Color.fromARGB(255, 225, 225, 225),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacement(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //       builder: (context) => const PetShowInfo()),
+                  // );
+                },
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.event,
+                      color: Colors.blueGrey,
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          "OMO 2 Load",
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  padding: const EdgeInsets.all(20),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  backgroundColor: const Color.fromARGB(255, 225, 225, 225),
+                ),
+                onPressed: () {
+                  // Navigator.pushReplacement(
+                  //   context,
+                  //   MaterialPageRoute(
+                  //       builder: (context) => const PetShowInfo()),
+                  // );
+                },
+                child: const Row(
+                  children: [
+                    Icon(
+                      Icons.event,
+                      color: Colors.blueGrey,
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: Text(
+                          "Neutral Tighting ",
+                          style: TextStyle(
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
