77c4f412c8350a019c3948c347fb7687015d3c42
zyn10 <zainandcs@gmail.com>
2023-06-29 01:37:54 +0500

    Merging left

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
     );
